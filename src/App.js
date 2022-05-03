fc912178560dcb6642f066e20046172bfc4b0f34
Bayoud Younesse <bayoudy@gmail.com>
2022-05-03 20:55:12 +0000

    commit changes for the stateful component projec

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 
 import './App.css';
-import Profile from './profile/Component';
+import Person from './Person/Component';
+
 
 
 function App() {
-  const handleName = (name) => alert(name);
 
-  return (
-    <div className="App">
-      <div
-        style={{
-          display: 'inline',
-          maxWidth: "55%",
-          borderRadius: "15px",
-          padding: "15px",
-        }}
-      >
-        <Profile
-          fullName={"Younesse Bayoud"}
-          profession={"Full Stack Web Developer"}
-          bio=" I'm a passionate Web Developer experienced  in Javascript, React,  html, css and other technoligies related to the back-end side  "
-          handleName={handleName}
-        >
-          <img
-            src="./image_profile.jpeg"
-            alt="This is my picature"
-            style={{ borderRadius: "15px", width: "300px" }}
-          />
-        </Profile>
-      </div>
-    </div>
-  );
+  return ( <div>
+    <h1 className='text-center'>I'm still Alive!</h1>
+    <Person />
+  </div>);
 }
 
-
-
 export default App;
+ 
+
+
+
